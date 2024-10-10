1ce88753e5dd7dab94e00dc07e77ce3a4296a455
jacquikeating <j.keating@live.ca>
2024-10-09 22:38:49 -0400

    Created pages and routes
    - Created folders and components for HomePage, AboutPage, ProjectPage, and ErrorPage.
    - Imported each into App.jsx.
    - Set up routes for each page with BrowserRouter.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,23 @@
-import { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import AboutPage from "./pages/AboutPage/AboutPage";
+import ProjectPage from "./pages/ProjectPage/ProjectPage";
+import ErrorPage from "./pages/ErrorPage/ErrorPage";
 import "./App.css";
 
 function App() {
-  return <></>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/about" element={<AboutPage />} />
+          <Route path="/projects/:projectName" element={<ProjectPage />} />
+          <Route path="/*" element={<ErrorPage />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/pages/ProjectPage/ProjectPage.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./ProjectPage.css";
+
+const ProjectPage = () => {
+  return <div>ProjectPage</div>;
+};
+
+export default ProjectPage;
