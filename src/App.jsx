6a7ad0eb5823e8379193730fe6e945c6c11b1896
jacquikeating <j.keating@live.ca>
2024-10-10 21:52:36 -0400

    Created Header and Footer components
    - Created Header and Footer components, along with associated folders and CSS files.
    - Imported into App and added to all pages.

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,22 @@ import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
 import ProjectPage from "./pages/ProjectPage/ProjectPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
   return (
     <>
       <BrowserRouter>
+        <Header />
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects/:projectName" element={<ProjectPage />} />
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </>
   );
