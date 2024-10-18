0a60e40bdcc1002b02aae33cb4eb30d538245179
jacquikeating <j.keating@live.ca>
2024-10-18 12:08:33 -0400

    Added ContactPage
    - Added ContactPage folder, component, and CSS file.
    - Imported ContactPage into App.jsx.
    - Added a <Route> for ContactPage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import HomePage from "./pages/HomePage/HomePage";
 import AboutPage from "./pages/AboutPage/AboutPage";
-import ProjectPage from "./pages/ProjectPage/ProjectPage";
+import ContactPage from "./pages/ContactPage/ContactPage";
 import ErrorPage from "./pages/ErrorPage/ErrorPage";
+import HomePage from "./pages/HomePage/HomePage";
+import ProjectPage from "./pages/ProjectPage/ProjectPage";
 import Header from "./components/Header/Header";
 import Footer from "./components/Footer/Footer";
 import "./App.css";
@@ -16,6 +17,7 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/about" element={<AboutPage />} />
           <Route path="/projects/:projectName" element={<ProjectPage />} />
+          <Route path="/contact" element={<ContactPage />} />
           <Route path="/*" element={<ErrorPage />} />
         </Routes>
         <Footer />
