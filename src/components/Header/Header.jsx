8e312d4c3897678186959958ac3c6d94b945a44c
jacquikeating <j.keating@live.ca>
2024-10-19 23:41:09 -0400

    Basic structure of Header component
    - Built basic HTML skeleton of Header component, including a <header>, <nav>, <ul>, <lis>, and <Links>.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,7 +2,31 @@ import React from "react";
 import "./Header.css";
 
 const Header = () => {
-  return <div>Header</div>;
+  return;
+  <header>
+    <nav className="nav">
+      <Link to="/" className="nav__logo">
+        Jacqui Keating
+      </Link>
+      <ul className="nav__list">
+        <li className="nav__list-item">
+          <Link to="/about" className="nav__link">
+            About
+          </Link>
+        </li>
+        <li className="nav__list-item">
+          <Link to="/projects" className="nav__link">
+            Projects
+          </Link>
+        </li>
+        <li className="nav__list-item">
+          <Link to="/contact" className="nav__link">
+            Contact
+          </Link>
+        </li>
+      </ul>
+    </nav>
+  </header>;
 };
 
 export default Header;
