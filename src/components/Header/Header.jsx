6ac1c7dc8c91a9b8ef722c8edb630a3e76d4bf90
jacquikeating <j.keating@live.ca>
2024-10-20 13:33:43 -0400

    Basic Header styling
    - Replaced Header.css with Header.scss.
    - Updated import for Header.jsx accordingly.
    - Applied basic styling, with all links aligned in a row (logo on the left, nav on the right).
    - Applied global style resets.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,32 +1,34 @@
 import React from "react";
-import "./Header.css";
+import { Link } from "react-router-dom";
+import "./Header.scss";
 
 const Header = () => {
-  return;
-  <header>
-    <nav className="nav">
-      <Link to="/" className="nav__logo">
-        Jacqui Keating
-      </Link>
-      <ul className="nav__list">
-        <li className="nav__list-item">
-          <Link to="/about" className="nav__link">
-            About
-          </Link>
-        </li>
-        <li className="nav__list-item">
-          <Link to="/projects" className="nav__link">
-            Projects
-          </Link>
-        </li>
-        <li className="nav__list-item">
-          <Link to="/contact" className="nav__link">
-            Contact
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  </header>;
+  return (
+    <header>
+      <nav className="nav">
+        <Link to="/" className="nav__logo">
+          Jacqui Keating
+        </Link>
+        <ul className="nav__list">
+          <li className="nav__list-item">
+            <Link to="/about" className="nav__link">
+              About
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/projects" className="nav__link">
+              Projects
+            </Link>
+          </li>
+          <li className="nav__list-item">
+            <Link to="/contact" className="nav__link">
+              Contact
+            </Link>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
 
 export default Header;
